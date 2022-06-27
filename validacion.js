38147ac4ca3293cb35d7e6dc2d798097a50ae3d6
Juan <juanleal.santafe@gmail.com>
2022-06-27 18:28:07 -0300

    Front-end

--- validacion.js
+++ validacion.js
@@ -1,1 +1,23 @@
-//Haz tú validación en javascript acá
+let parrafo = document.getElementById('invisible');
+let boton01 = document.getElementById('boton');
+
+boton01.addEventListener('click',function(){
+
+    parrafo.style.display = 'flex';
+    boton01.style.display = 'none';
+
+})
+
+let parrafo2 = document.getElementById('invisible2');
+let boton02 = document.getElementById('boton2');
+
+
+boton02.addEventListener('click',function(event){
+
+    event.preventDefault();
+    parrafo2.style.display = 'flex';
+    boton02.style.display = 'none';
+
+})
+
+
